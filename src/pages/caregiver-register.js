1f3535f5a685f9b1c618eb90cbaa34864f018179
Nilesh Bhardwaj <94599253+nileshrajbhar731@users.noreply.github.com>
2023-02-04 15:24:12 +0530

    remove

--- src/pages/caregiver-register.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import MemberRegistration from '../components/Auth/Registration/MemberRegistration'
-
-export default function CaregiverRegisterPage() {
-    return (
-        <MemberRegistration registeringCaregiver={true}/>
-    )
-}
